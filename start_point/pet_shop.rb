58e3de6c17059ca11f285f322e555bf6329f3099
Catriona Ford <catriona.b.ford@gmail.com>
2018-10-19 13:42:27 +0100

    test 4 add_or_remove_cash__remove passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,9 +1,9 @@
 def pet_shop_name(pet_shop)
-  return pet_shop[:name]
+  pet_shop[:name]
 end
 
 def total_cash(pet_shop)
-  return pet_shop[:admin][:total_cash]
+  pet_shop[:admin][:total_cash]
 end
 
 def add_or_remove_cash(pet_shop, cash_amount)
